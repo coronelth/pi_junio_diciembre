b0323d6a9a1b06e31525771af218624e42d62443
Thomas Coronel <coronelth@mecom26.cabib.local>
2019-09-25 15:59:26 -0300

    Hasta esta instancia se realizo el codigo en c, donde se abre las posiciones de los vecinos de un archivo txt los cuales fueron generados previamente. La forma de guardar los vecinos corresponden a un patron que empieza con una cruz y sigue por las diagonales.

---
+++ c_codigo/lattice_reducido.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+
+// Funciones de alocacion y liberaci´on de memoria
+
+double *allocaVector(double n) {
+	double *v = (double *) malloc(n * sizeof(double));
+	assert(v != NULL);
+	return v;
+}
+
+void liberaVector(double *vec) {
+free(vec);
+return;
+}
+
+//matriz distribicion y matriz de suma
+void liberaMatriz(double** pmat, int node) {
+	int i;
+	for( i=0; i<node; i++)
+		free(pmat[i]);
+		free(pmat);
+	return;
+	}
+double ** allocaMatriz(int node, int nveloc) {	
+	int i;
+	double **pmat;
+	pmat = (double **) malloc(node * sizeof(double*));
+	if( pmat == NULL )
+	return NULL; 
+
+	for( i=0; i<node; i++){
+		pmat[i]=(double *) malloc(nveloc * sizeof(double));
+			if( pmat[i] == NULL ){
+				liberaMatriz(pmat,i);
+	return NULL;
+	}
+	}
+	return pmat;
+}
+
+
+//matriz de vecinos
+void liberaVecinos(int ** pmat, int node) {
+	int i;
+	for( i=0; i<node; i++)
+		free(pmat[i]);
+		free(pmat);
+	return;
+	}
+
+int ** allocaVecinos(int node, int nvecinos) {	
+	int i;
+	int **pmat;
+	pmat = (int **) malloc(node * sizeof(int*));
+	if( pmat == NULL )
+	return NULL; 
+
+	for( i=0; i<node; i++){
+		pmat[i]=(int *) malloc(nvecinos * sizeof(int));
+			if( pmat[i] == NULL ){
+				liberaVecinos(pmat,i);
+	return NULL;
+	}
+	}
+	return pmat;
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// LLenar Vecinos
+
+void llenarVecinos(int ** pmat, int row, int colum){
+	FILE *fichero;
+	int node=row*colum;
+	int i,j;
+	int nvec=9;
+		
+
+	
+ 	fichero = fopen("matriz_con_vecinos.txt","r");
+	
+	   if (fichero==NULL)
+   	{
+  	    printf( "No se puede abrir el fichero.\n" );
+	      system("pause");
+	      exit (EXIT_FAILURE);
+	   }
+	
+	for(i=0;i<node;i++){
+
+		for(j=0;j<nvec;j++){
+
+			fscanf(fichero,"%i",&pmat[i][j]);	
+		}
+	fscanf(fichero, "\n"); 
+	}
+
+
+       
+   fclose(fichero);
+/*
+---------------------------------------------------- COMPROBACION DE QUE ESTA LEYENDO CORRECTAMENTE-----------------------------------
+   printf( "Contenido del fichero:\n" );
+   for (i = 0; i < node; i++) {
+      for (j = 0; j < nvec; j++)
+	 printf ("%d ", pmat[i][j]);
+      printf ("\n");
+   }
+*/
+   return ;
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// Llenar velocidad
+
+void llenarVelocidad(double ** pmat, int node){
+	int i,j;	
+	for(i=0;i<node;i++){
+		for(j=0;j<9;j++){
+			if (j==0)
+			pmat[i][j]=0;
+						
+		pmat[i][j]=drand48();
+		}
+	}
+}
+
+	
+// Sumar velocidades
+
+void sumarVelocidad(double ** pdist,int ** pvec,double ** psum, int node){
+	int i,j,k;	
+	
+	for(i=0;i<node;i++)	//para cada nodo
+		for(j=0;j<9;j++)	//para cada vecino (son 9)
+			for(k=0;k<9;k++){	//para cada velocidad
+			
+			psum[i][k]+=pdist[pvec[i][j]][k];			
+	}	
+}
+
+//*****************-----------------------------********************************--------------------*********************************
+
+int main(void){
+
+
+	
+	
+// Definici´on de las dimensiones de mi problema
+	int row=6;
+	int colum=6;
+	int node= row*colum;
+	int nveloc=9;
+	int nvec=9;
+	
+	clock_t t_ini, t_fin;
+	double secs;
+
+// Alocaci´on de las matrices de mi problema	
+	double** matdist = allocaMatriz(node,nveloc);
+	double** matsum = allocaMatriz(node,nveloc);
+	int** matvec = allocaVecinos(node,nvec);
+
+//Inicializaci´on de los valores
+	llenarVelocidad(matdist,node);
+	llenarVecinos(matvec,row,colum);	
+
+//Suma de los valores de la velocidad
+// Tomas de tiempo
+
+	t_ini = clock();
+	sumarVelocidad(matdist,matvec,matsum,node);
+	t_fin = clock();
+	
+
+ 	secs = (double)(t_fin - t_ini) / CLOCKS_PER_SEC;
+	printf("%.16g milisegundos\n", secs * 1000.0);   	
+
+
+//Liberaci´on de memoria
+
+liberaMatriz(matdist,node);
+liberaMatriz(matsum,node);
+liberaVecinos(matvec,node);
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ generar_matriz_vecinos.c
@@ -0,0 +1,114 @@
+// Codigo para realizar un .txt donde tenga guardado los vecinos de los puntos de la grilla
+//Para ello tengo que pasarle como argumento el numero de columnas y filas de mi grilla
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+int main (void){
+
+// donde row y colum son del problema fisico real que me marcan la grilla
+
+//Declaracion de variables	
+//----------------------------------------------------------------
+	int row=6;
+	int colum=6;
+	int node=row*colum;
+	int nvec=9; // son la cantidad de vecinos que puede tener un punto de la grilla (contandose a si mismo)
+	
+	int i,j;
+	int** pmat;
+	
+//Allocacion de la matriz con los parametros
+//-----------------------------------------------------------------
+	pmat=(int **) malloc(node * sizeof(int*));
+
+	for( i=0; i<node; i++){
+		pmat[i]=(int *) malloc(nvec * sizeof(int));
+	}
+
+//Empieza el rellenado de los vecinos, esto tiene que ver con la primera forma (cruz y luego diagonales)
+	
+	for(i=0;i<node;i++){
+				
+			// rellenado para un lugar en el centro
+			pmat[i][0]=i;
+			pmat[i][1]=i+1;
+			pmat[i][2]=i+colum;
+			pmat[i][3]=i-1;
+			pmat[i][4]=i-colum;
+			pmat[i][5]=i+colum+1;
+			pmat[i][6]=i+colum-1;
+			pmat[i][7]=i-colum-1;
+			pmat[i][8]=i-colum+1;
+
+			// se encuentra en el borde izquierdo
+			if ((i%colum)==0){
+				pmat[i][3]=0;
+				pmat[i][6]=0;
+				pmat[i][7]=0;
+			}
+
+			// se encuentra en el borde derecho
+			if (((i+1)%colum)==0){
+				pmat[i][1]=0;
+				pmat[i][5]=0;
+				pmat[i][8]=0;
+			}
+
+			// se encuentra en el borde inferior
+			if (i < colum) {
+				pmat[i][4]=0;
+				pmat[i][7]=0;
+				pmat[i][8]=0;
+			}
+
+			// se encuentra en el borde superior
+			if ((i<(colum*row)) && (i>=(colum*row-colum))){
+				pmat[i][2]=0;
+				pmat[i][5]=0;
+				pmat[i][6]=0;
+			}
+			// con la implementacion de estas cuatro condiciones tambien tengo incluido las puntas (hay que ver bien que valor adoptan si 0 o -1
+			// porque el valor de 0 me da la direccion de uno de los nodos, hay que ver la implementacion del -1 y que pasa en el resto del codigo)			
+
+	}
+
+// Guardar el archivo en un .txt
+//-----------------------------------------------
+
+
+FILE *f = fopen("matriz_con_vecinos.txt", "w");
+if (f == NULL)
+{
+    printf("Error opening file!\n");
+    exit(1);
+}
+
+
+for(i=0;i<node;i++){
+	for(j=0;j<nvec;j++){
+		fprintf(f,"%i\t",pmat[i][j] );
+		}
+		
+	fprintf(f,"\n");
+	
+}
+
+fclose(f);
+
+
+//Liberacion memoria
+
+	for( i=0; i<node; i++){
+		free(pmat[i]);
+		//free(pmat);
+	}
+//--------------------------------------------------
+	return 0;
+
+
+
+}
