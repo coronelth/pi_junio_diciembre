723d73829062eeae6e296d4d58c3bafd91bf0641
Thomas Coronel <coronelth@mecom26.cabib.local>
2019-10-23 17:47:37 -0300

    En el codigo para cuda se realizo la inclusion de la medicion del tiempo de calculo de la suma de los elementos de la matriz de distribucion, hay que ver de que este leyendo bien los valores que tiene que cargar, hay un error en la implementacion del kernel sumavelocidad , una vez resuelto esto se puede empezar a comparar los tiempos con el codigo de c.

--- cuda_codigo/cu_codigo.cu
+++ cuda_codigo/cu_codigo.cu
@@ -5,14 +5,13 @@
 #include <time.h>
 #include <cuda_runtime.h>
 
-//-------------Funcion llenar la "velocidad"
+//-------------Funcion llenar "velocidad"
 
 void llenarVelocidad(double ** pmat, int row, int colum){
 	FILE *fichero;
 	int node=row*colum;
 	int i,j;
 	int nvel=9;
-	int height, width;
 	float leer;
 	
  	fichero = fopen("matriz_con_func_dist.txt","r");
@@ -30,11 +29,11 @@ void llenarVelocidad(double ** pmat, int row, int colum){
 
 			fscanf(fichero,"%f",&leer);	
 			pmat[i][j]=leer;
-			//printf("%f",leer);
+			printf("%f",leer);
 		}
 	fscanf(fichero, "\n"); 
 	}
-	
+
 	fclose(fichero);
 }
 
@@ -70,31 +69,29 @@ void llenarVecinos(int ** pmat, int row, int colum){
 
        
    fclose(fichero);
-
+}
 
 //-------------Funcion sumar velocidad
 
-__device__ void sumarvelocidad(double ** pdist,int ** pvec,double ** psum, int node) {
+__global__ void sumarvelocidad(double ** pdist,int ** pvec,double ** psum, int node) {
 int nvec=9;	//numero de vecinos
 int ndist=9;	//numero de funcion de distribucion
-
+int k;
 int x = threadIdx.x + blockIdx.x * blockDim.x;
 int y = threadIdx.y + blockIdx.y * blockDim.y;
-int offset = x + y * blockDim.x * gridDim.x;
-
-int j;
+// int offset = x + y * blockDim.x * gridDim.x;
 
 if (x<node){ //para que se paralelice en cada nodo
 	if (y<nvec){	//para que se paralelice en cada vecino
-		for(k=0;k<nvel;k++){	//para cada velocidad realizo la suma al no saber como paralelizar esta parte
-			psum[i][k]+=pdist[pvec[i][j]][k];				
+		for(k=0;k<ndist;k++){	//para cada velocidad realizo la suma al no saber como paralelizar esta parte
+			psum[x][k]+=pdist[pvec[x][y]][k];				
+						}
 		}
+
+
+		}
+
 }
-
-
-}
-
-
 
 // MAIN: rutina principal ejecutada en el host
 int main(int argc, char** argv)
@@ -105,34 +102,74 @@ int row=6;
 int colum=6;
 int node=row*colum;
 int nvec=9;
+int nvel=9;
 
 int *dev_vecinos;
-float *dev_matriz;
-float *hst_matriz;
+double *dev_velocidad;
+double *dev_suma;
+double *hst_velocidad;
 
 
 // reserva en el host
-hst_matriz = (float*)malloc( node*nvec*sizeof(float) );
+hst_velocidad = (double*)malloc( node*nvel*sizeof(double) );
 
 // reserva en el device
-cudaMalloc( (void**)&dev_matriz, node*nvec*sizeof(float) );
+cudaMalloc( (void**)&dev_velocidad, node*nvel*sizeof(double) );
+cudaMalloc( (void**)&dev_suma, node*nvel*sizeof(double) );
 cudaMalloc( (void**)&dev_vecinos, node*nvec*sizeof(int) );
 
 // inicializacion de datos
 
-llenarVecinos(int ** dev_vecinos, int row, int colum);
-llenarVelocidad(double ** dev_matriz, int row, int colum);
+llenarVecinos(&dev_vecinos, row, colum);
+llenarVelocidad(&dev_velocidad, row, colum);
 
-/*
+
+// declaracion de eventos
+cudaEvent_t start;
+cudaEvent_t stop;
+// creacion de eventos
+cudaEventCreate(&start);
+cudaEventCreate(&stop);
+// marca de inicio
+cudaEventRecord(start,0);
+// codigo a temporizar en el device
+
+// -----------------------------------------------
+	sumarvelocidad<<<16,256>>>(&dev_velocidad, &dev_vecinos, &dev_suma, node);
+   if (cudaDeviceSynchronize() != cudaSuccess) {
+       fprintf (stderr, "Cuda call failed\n");
+   }
+
+// aqui va el kernel que realiza la suma que es lo que se quiere medir
+// -----------------------------------------------
+
+
+// marca de final
+cudaEventRecord(stop,0);
+// sincronizacion GPU-CPU
+cudaEventSynchronize(stop);
+// calculo del tiempo en milisegundos
+float elapsedTime;
+cudaEventElapsedTime(&elapsedTime,start,stop);
+// impresion de resultados
+printf("> Tiempo de ejecucion: %f ms\n",elapsedTime);
+// liberacion de recursos
+cudaEventDestroy(start);
+cudaEventDestroy(stop);
+
+
+
+
+
+
 // copia de datos
-cudaMemcpy(dev_matriz, hst_matriz, N*N*sizeof(float), cudaMemcpyHostToDevice);
+cudaMemcpy(hst_velocidad, dev_velocidad, node*nvel*sizeof(float), cudaMemcpyDeviceToHost);
 // salida
-*/
 
 
-cudaFree( dev_vecinos )
-cudaFree( dev_matriz )
-cudaFree( hst_matriz )
+cudaFree( dev_vecinos );
+cudaFree( dev_velocidad );
+cudaFree( hst_velocidad );
 
 
 return 0;
